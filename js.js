a9570b74b82d362d2bcb5e9d3c2de3692fab1990
Adi Campan <adicampan1974@gmail.com>
2022-03-05 09:53:24 +0100

    add converter

--- js.js
+++ js.js
@@ -1,53 +1,54 @@
    
-   
-   
-                 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                        const d = new Date();
-                        let day = weekday[d.getDay()];
-                        let hour = d.getHours();
-                        let min= d.getMinutes();   
+                  
 function getForecastData(lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=7a13b39d6e8e0ad7bc6d10fed36b42d1&units=metric")
         .then(function(res) {
             return res.json();
         })
         .then(function(data) {
-            console.log(data); 
-            console.log(data.daily[0].temp.day); 
-                 
-            document.getElementById("day1").innerText = Math.round (data.daily[1].temp.day);
-            document.getElementById("day1").innerText = Math.round (data.daily[1].temp.night);
-                const z = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                var luni = (new Date(data.daily[1].dt * 1000)).getDay();
-                var marti = (new Date(data.daily[2].dt * 1000)).getDay();
-                var miercuri = (new Date(data.daily[3].dt * 1000)).getDay();
-                var joi = (new Date(data.daily[4].dt * 1000)).getDay();
-                var vineri = (new Date(data.daily[5].dt * 1000)).getDay();
-                var sambata = (new Date(data.daily[6].dt * 1000)).getDay();
-                var duminica = (new Date(data.daily[7].dt * 1000)).getDay();
+          
+            const z = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+            for (var i=1; i<data.daily.length; i++ ){
+                var numeZi = z[(new Date(data.daily[i].dt * 1000)).getDay()];               
+                var dayElement = document.createElement("div");
+                var icon2 = document.createElement("IMG");
                 
-           
-            document.getElementById("luni").innerText = z[luni];
-            document.getElementById("marti").innerText = z[marti];
-            document.getElementById("miercuri").innerText = z[miercuri];
-            document.getElementById("joi").innerText = z[joi];
-            document.getElementById("vineri").innerText = z[vineri];
-            document.getElementById("sambata").innerText = z[sambata];
-            document.getElementById("duminica").innerText = z[duminica];
-            
+                var nameElement = document.createElement("div");
+                var maxElement = document.createElement("div");
+                var minElement = document.createElement("div");
+                 var minMax = document.createElement("div");
+                
+                maxElement.innerText = Math.round(data.daily[i].temp.max);
+                maxElement.className = "max";
+                nameElement.innerText = numeZi;
+                nameElement.className = "name";
+                minElement.innerText = Math.round(data.daily[i].temp.min);
+                minElement.className = "min";
+                
+                var iconcod = data.daily[i].weather[0].icon;
+                var iconurl2 = "http://openweathermap.org/img/w/" + iconcod + ".png";
+                icon2.src = iconurl2;
+
+                dayElement.appendChild(nameElement);
+                dayElement.appendChild(icon2);
+                dayElement.appendChild(minMax);
+                minMax.appendChild(maxElement);
+                minMax.appendChild(minElement);   
+                icon2.className = "icon2";
+                minMax.className = "minMax";
+
+                dayElement.className = "day";
+                document.getElementsByClassName("week")[0].appendChild(dayElement);
+            }            
         })
         .catch(function(error) {
             console.log(error);
         })
-                // var dayNames = ["duminica", "luni", "marti", "miercuri", "joi", "vineri", "sambata"];
-                //     for(i=0; i<dayNames.length; i++){
-                //         new Date(dayNames[i].dt * 1000)
-                //             //console.log(dayNames[i]);
-                //     }
-        
+  
 }
 
-      
+           
+var temp;      
 
 function search(){
     var ciudad = document.getElementById("city").value;
@@ -58,12 +59,22 @@ function search(){
         })
         .then(function(data) {
             console.log(data);
-            console.log(data.main.temp);
+            
+
             var iconcode= data.weather[0].icon;
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
             document.getElementById("icon").src = iconurl;
-            var temp=Math.round (data.main.temp);
+
+            
+            
+            temp = Math.round(data.main.temp);
             document.getElementById("temp").innerText = temp + "\u2103";
+
+            const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+                        const d = new Date();
+                        let day = weekday[d.getDay()];
+                        let hour = d.getHours();
+                        let min= d.getMinutes(); 
                     
             document.getElementById("day").innerHTML = day + " " +  hour +":" + min;
             document.getElementById("rain").innerText = data.main.humidity + "%";
@@ -79,12 +90,12 @@ function search(){
             console.log(error);
         })
 }
-
-
-
-
-//3
-
-// dayNames[day - 1]
-
-//new Date(asda[i].dt * 1000)
+function faren(){
+             var fahrenheit=Math.round((temp*9)/5)+32;
+             document.getElementById("temp").innerText = fahrenheit + " \u2109";
+                  
+}
+function celsius(){
+    document.getElementById("temp").innerText = temp +"\u2103";
+    console.log(temp);
+}
