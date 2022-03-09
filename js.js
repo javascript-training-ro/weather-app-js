d9095ba278605d0474c412fe36eea1bfbb8a1a42
Adi Campan <adicampan1974@gmail.com>
2022-03-10 00:03:02 +0100

    Add wind icon and UV bar progress

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
    
-                  
+document.getElementById("city").value = localStorage.getItem("orasul");
+console.log('aici');
+
 function getForecastData(lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=7a13b39d6e8e0ad7bc6d10fed36b42d1&units=metric")
         .then(function(res) {
@@ -8,14 +10,34 @@ function getForecastData(lat, lon) {
         })
         .then(function(data) {
           console.log(data);
-             hourly = data.hourly[15].temp;
+
+            
+          var iconcode= data.current.weather[0].icon;
+          var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+          document.getElementById("icon").src = iconurl;
+
+          
+          
+          temp = Math.round(data.current.temp);
+          document.getElementById("temp").innerText = temp + "\u2103";
+
+          const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+                      const d = new Date();
+                      let day = weekday[d.getDay()];
+                      let hour = d.getHours();
+                      let min= d.getMinutes(); 
+                  
+          document.getElementById("day").innerHTML = day + " " +  hour +":" + min;
+          document.getElementById("rain").innerText = "Humidity " + data.current.humidity + "%";
+          document.getElementById("cloudy").innerText = data.current.weather[0].main; 
+
           
             const z = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
             for (var i=1; i<data.daily.length; i++ ){
                 var numeZi = z[(new Date(data.daily[i].dt * 1000)).getDay()];               
-                var dayElement = document.createElement("div");
-                var icon2 = document.createElement("IMG");
                 
+                var dayElement = document.createElement("div");
+                var icon2 = document.createElement("IMG");                
                 var nameElement = document.createElement("div");
                 var maxElement = document.createElement("div");
                 var minElement = document.createElement("div");
@@ -46,38 +68,80 @@ function getForecastData(lat, lon) {
                 dayElement.className = "day";
                 document.getElementsByClassName("week")[0].appendChild(dayElement);
             }
+             
+
+            showHourlyData(data.hourly);
 
 
             var indiceuv = document.createElement("div");
             var wind = document.createElement("div");
+            var windDirection = document.createElement("img");
             var sr = document.createElement("div");
             var ss = document.createElement("div");
             var hum = document.createElement("div");
             var vis = document.createElement("div");
-            var air = document.createElement("div");
-            
-            indiceuv.id = "indiceUV";               
+            var air = document.createElement("div");            
+            indiceuv.id = "indiceUV";         
             wind.id = "windSpeed";
+            windDirection.id = "direction";
+            windDirection.src = "direction.png";
+            windDirection.style.transform = "rotate(" + data.current.wind_deg + "deg)";
             sr.id = "sunrise";
             ss.id = "sunset";
             hum.id = "humidity";
             vis.id = "visibility";
             air.id = "airQ"
             div1.appendChild(indiceuv);
-            div2.appendChild(wind);               
+            div2.appendChild(windDirection);            
+            div2.appendChild(wind);
             div3.appendChild(sr);
             div3.appendChild(ss);
             div4.appendChild(hum);
             div5.appendChild(vis);
             div6.appendChild(air);
+
             document.getElementById("indiceUV").innerText = data.daily[0].uvi;
+            document.getElementById("progress").style = "width:" + ((data.daily[0].uvi)*7) + "%";
             document.getElementById("windSpeed").innerText = Math.round((data.daily[0].wind_speed * 3600) / 1000)  + " Km/h";
             document.getElementById("sunrise").innerText = (new Date(data.daily[0].sunrise * 1000)).getHours() + ":" + (new Date(data.daily[0].sunrise * 1000)).getMinutes();
             document.getElementById("sunset").innerText = (new Date(data.daily[0].sunset * 1000)).getHours() + ":" + (new Date(data.daily[0].sunset * 1000)).getMinutes();
             document.getElementById("humidity").innerText = (data.daily[0].humidity) + "%";
             document.getElementById("visibility").innerText = (data.current.visibility)/1000 + " Km";
             document.getElementById("airQ").innerText = data.daily[0].dew_point;
-        
+
+            if (data.current.wind_deg < 23 && data.current.wind_deg > 123 ) {
+                    document.getElementById("direccion").innerText = "N";
+            }else if (data.current.wind_deg < 33.75 && data.current.wind_deg > 11.25 ){
+                    document.getElementById("direccion").innerText = "NNE";
+            }else if (data.current.wind_deg < 56.25 && data.current.wind_deg > 33.75 ){
+                    document.getElementById("direccion").innerText = "NE";
+            }else if (data.current.wind_deg < 78.75 && data.current.wind_deg > 56.25 ){
+                    document.getElementById("direccion").innerText = "ENE";
+            }else if (data.current.wind_deg < 101.25 && data.current.wind_deg > 78.75 ){
+                document.getElementById("direccion").innerText = "E";
+            }else if (data.current.wind_deg < 123.75 && data.current.wind_deg > 101.25 ){
+                document.getElementById("direccion").innerText = "ESE";
+            }else if (data.current.wind_deg < 146.25 && data.current.wind_deg > 123.75 ){
+                document.getElementById("direccion").innerText = "SE";
+            }else if (data.current.wind_deg < 168.75 && data.current.wind_deg > 146.25 ){
+                document.getElementById("direccion").innerText = "SSE";
+            }else if (data.current.wind_deg < 191.25 && data.current.wind_deg > 168.75 ){
+                document.getElementById("direccion").innerText = "S";
+            }else if (data.current.wind_deg < 213.75 && data.current.wind_deg > 191.25 ){
+                document.getElementById("direccion").innerText = "SSW";
+            }else if (data.current.wind_deg < 236.25 && data.current.wind_deg > 213.75 ){
+                document.getElementById("direccion").innerText = "SW";
+            }else if (data.current.wind_deg < 258.75 && data.current.wind_deg > 236.25 ){
+                document.getElementById("direccion").innerText = "WSW";
+            }else if (data.current.wind_deg < 281.25 && data.current.wind_deg > 258.75 ){
+                document.getElementById("direccion").innerText = "W";
+            }else if (data.current.wind_deg < 303.75 && data.current.wind_deg > 281.25 ){
+                 document.getElementById("direccion").innerText = "WNW";
+            }else if (data.current.wind_deg < 326.25 && data.current.wind_deg > 303.75 ){
+                 document.getElementById("direccion").innerText = "NW";
+            }else if (data.current.wind_deg < 348.75 && data.current.wind_deg > 326.25 ){
+                document.getElementById("direccion").innerText = "NNW";
+            }        
         })
         .catch(function(error) {
             console.log(error);
@@ -85,13 +149,28 @@ function getForecastData(lat, lon) {
   
 }
 
-var hourly;        
-var temp;      
+     
 
-function search(){
+function search(lat, long) {
     var ciudad = document.getElementById("city").value;
+    localStorage.setItem("orasul", ciudad); 
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=7a13b39d6e8e0ad7bc6d10fed36b42d1&units=metric")
+    console.log(ciudad); 
+    
+
+    document.getElementById("saptamana").innerText = "";
+    document.getElementById("today").innerText = "";
+
+    var searchQuery = '';
+
+    if (lat && long) {
+        searchQuery = 'lat=' + lat + '&lon=' + long;
+    } else {
+        searchQuery = 'q=' + ciudad;
+    }
+
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?" + searchQuery + "&appid=7a13b39d6e8e0ad7bc6d10fed36b42d1&units=metric")
         .then(function(res) {
             return res.json();
         })
@@ -99,28 +178,12 @@ function search(){
             console.log(data);
             
 
-            var iconcode= data.weather[0].icon;
-            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            document.getElementById("icon").src = iconurl;
-
-            
-            
-            temp = Math.round(data.main.temp);
-            document.getElementById("temp").innerText = temp + "\u2103";
-
-            const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                        const d = new Date();
-                        let day = weekday[d.getDay()];
-                        let hour = d.getHours();
-                        let min= d.getMinutes(); 
-                    
-            document.getElementById("day").innerHTML = day + " " +  hour +":" + min;
-            document.getElementById("rain").innerText = data.main.humidity + "%";
-            document.getElementById("cloudy").innerText = data.weather[0].main; 
+           document.getElementById('city').value = data.name + ', ' + data.sys.country;
 
             // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=7a13b39d6e8e0ad7bc6d10fed36b42d1&units=metric
               
             getForecastData(data.coord.lat, data.coord.lon);
+            
 
             
 
@@ -129,6 +192,14 @@ function search(){
         .catch(function(error) {
             console.log(error);
         })
+}
+function getLocation(){
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+        search(position.coords.latitude, position.coords.longitude);
+    }, function (error) {
+        console.log('eroare', error);
+    });
 }
 function faren(){
              var fahrenheit=Math.round((temp*9)/5)+32;
@@ -139,16 +210,32 @@ function celsius(){
     document.getElementById("temp").innerText = temp +"\u2103" ;
     
 }
-function day(){
-    
-    document.getElementById("saptamana").style.display = 'none';
-    document.getElementById("today").style.display = "flex";
-    document.getElementById("today").innerText = hourly;
-    console.log(hourly);
 
+function showHourlyData (hourlyData) {
+
+    // console.log('hourly', hourlyData);
+    for(var i=2 ; i<=14; i+=2){
+        var mainDiv = document.createElement('div');
+       
+        var divHora = document.createElement('div');
+        divHora.innerText = (new Date(hourlyData[i].dt * 1000)).getHours() + ":" + (new Date(hourlyData[i].dt * 1000)).getMinutes() + "0";
+        var divIcon = document.createElement("img");
+        divIcon.src = "http://openweathermap.org/img/w/" + hourlyData[i].weather[0].icon  + ".png";
+        var divTemp = document.createElement("div");
+        divTemp.innerText = Math.round(hourlyData[i].temp) + "\u2103";
+
+        mainDiv.append(divHora,divIcon,divTemp);
+        mainDiv.className = "hour";
+        today.appendChild(mainDiv);
+        
+        
+    }
+}
+
+function day(){
+    document.getElementsByClassName("details")[0].classList.remove("saptamana");
 }
 
 function week() {
-    document.getElementById("saptamana").style.display = 'flex';
-    document.getElementById("today").style.display = "none";
+    document.getElementsByClassName("details")[0].classList.add("saptamana");  
 }
